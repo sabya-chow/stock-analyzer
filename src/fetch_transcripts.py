cf22b6594976535c6dae0d3807e56570fc8cb7f2
sabyachow <sabyachow@gmail.com>
2025-06-19 18:30:50 +0530

    updated_fetch_transcript

--- src/fetch_transcripts.py
+++ src/fetch_transcripts.py
@@ -1,5 +1,16 @@
-from langchain.document_loaders import PyPDFLoader
+import os
+from langchain_community.document_loaders import PyPDFLoader
 
-def load_transcript(path):
-    return PyPDFLoader(path).load()
-# Functions for loading corporate transcripts using LangChain
+def load_transcript_texts(raw_folder="../data/raw"):
+    raw_folder = os.path.abspath(raw_folder)  # ← NEW: ensure it's absolute
+    pdf_files = [f for f in os.listdir(raw_folder) if f.endswith(".pdf")]
+    docs = []
+
+    for file in pdf_files:
+        path = os.path.join(raw_folder, file)
+        loader = PyPDFLoader(path)
+        pages = loader.load()
+        docs.extend(pages)
+
+    return docs
+
